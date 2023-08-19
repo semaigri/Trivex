b7c2720ff1bdd93ba3223502dd5b1de23b2b937d
semaigri <72685197+semaigri@users.noreply.github.com>
2023-08-19 16:04:01 +0300

    Update heavyhash.go

--- domain/consensus/utils/pow/heavyhash.go
+++ domain/consensus/utils/pow/heavyhash.go
@@ -1,91 +1,65 @@
-package pow
+// ethash: C/C++ implementation of Ethash, the Ethereum Proof of Work algorithm.
+// Copyright 2018-2019 Pawel Bylica.
+// Licensed under the Apache License, Version 2.0.
+
+package firopow
 
 import (
-	"github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
-	"github.com/kaspanet/kaspad/domain/consensus/utils/hashes"
-	"math"
+	"encoding/binary"
+
+	"github.com/sencha-dev/powkit/internal/common/convutil"
+	"github.com/sencha-dev/powkit/internal/crypto"
+	"github.com/sencha-dev/powkit/internal/progpow"
 )
 
-const eps float64 = 1e-9
-
-type matrix [64][64]uint16
-
-func generateMatrix(hash *externalapi.DomainHash) *matrix {
-	var mat matrix
-	generator := newxoShiRo256PlusPlus(hash)
-	for {
-		for i := range mat {
-			for j := 0; j < 64; j += 16 {
-				val := generator.Uint64()
-				for shift := 0; shift < 16; shift++ {
-					mat[i][j+shift] = uint16(val >> (4 * shift) & 0x0F)
-				}
-			}
-		}
-		if mat.computeRank() == 64 {
-			return &mat
-		}
+func initialize(hash []byte, nonce uint64) ([25]uint32, uint64) {
+	var seed [25]uint32
+	for i := 0; i < 8; i += 1 {
+		seed[i] = binary.LittleEndian.Uint32(hash[i*4 : i*4+4])
 	}
+
+	seed[8] = uint32(nonce)
+	seed[9] = uint32(nonce >> 32)
+	seed[10] = 0x00000001
+	seed[18] = 0x80008081
+
+	crypto.KeccakF800(&seed)
+
+	seedHead := uint64(seed[0]) + (uint64(seed[1]) << 32)
+
+	return seed, seedHead
 }
 
-func (mat *matrix) computeRank() int {
-	var B [64][64]float64
-	for i := range B {
-		for j := range B[0] {
-			B[i][j] = float64(mat[i][j])
-		}
+func finalize(seed [25]uint32, mixHash []byte) []byte {
+	var state [25]uint32
+	for i := 0; i < 8; i++ {
+		state[i] = seed[i]
+		state[i+8] = binary.LittleEndian.Uint32(mixHash[i*4 : i*4+4])
 	}
-	var rank int
-	var rowSelected [64]bool
-	for i := 0; i < 64; i++ {
-		var j int
-		for j = 0; j < 64; j++ {
-			if !rowSelected[j] && math.Abs(B[j][i]) > eps {
-				break
-			}
-		}
-		if j != 64 {
-			rank++
-			rowSelected[j] = true
-			for p := i + 1; p < 64; p++ {
-				B[j][p] /= B[j][i]
-			}
-			for k := 0; k < 64; k++ {
-				if k != j && math.Abs(B[k][i]) > eps {
-					for p := i + 1; p < 64; p++ {
-						B[k][p] -= B[j][p] * B[k][i]
-					}
-				}
-			}
-		}
-	}
-	return rank
+
+	state[17] = 0x00000001
+	state[24] = 0x80008081
+
+	crypto.KeccakF800(&state)
+
+	return convutil.Uint32ArrayToBytes(state[:8], binary.LittleEndian)
 }
 
-func (mat *matrix) HeavyHash(hash *externalapi.DomainHash) *externalapi.DomainHash {
-	hashBytes := hash.ByteArray()
-	var vector [64]uint16
-	var product [64]uint16
-	for i := 0; i < 32; i++ {
-		vector[2*i] = uint16(hashBytes[i] >> 4)
-		vector[2*i+1] = uint16(hashBytes[i] & 0x0F)
-	}
-	// Matrix-vector multiplication, and convert to 4 bits.
-	for i := 0; i < 64; i++ {
-		var sum uint16
-		for j := 0; j < 64; j++ {
-			sum += mat[i][j] * vector[j]
-		}
-		product[i] = sum >> 10
+func firopow(hash []byte, height, nonce, datasetSize uint64, lookup func(index uint32) []uint32, l1 []uint32) ([]byte, []byte) {
+	cfg := &progpow.Config{
+		PeriodLength:        1,
+		DagLoads:            4,
+		CacheBytes:          16 * 1024,
+		LaneCount:           16,
+		RegisterCount:       32,
+		RoundCount:          64,
+		RoundCacheAccesses:  11,
+		RoundMathOperations: 18,
 	}
 
-	// Concatenate 4 LSBs back to 8 bit xor with sum1
-	var res [32]byte
-	for i := range res {
-		res[i] = hashBytes[i] ^ (byte(product[2*i]<<4) | byte(product[2*i+1]))
-	}
-	// Hash again
-	writer := hashes.NewHeavyHashWriter()
-	writer.InfallibleWrite(res[:])
-	return writer.Finalize()
+	seed, seedHead := initialize(hash, nonce)
+	mixHash := progpow.Hash(cfg, height, seedHead, datasetSize, lookup, l1)
+	digest := finalize(seed, mixHash)
+
+	return mixHash, digest
 }
